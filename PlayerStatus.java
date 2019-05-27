bd0e6a8ab9c3d509ed27a04eba7a3869086d42c2
jccccc6 <42724771+jccccc6@users.noreply.github.com>
2019-05-27 11:22:46 -0700

    created whichplayer method
    changes which player can go

--- PlayerStatus.java
+++ PlayerStatus.java
@@ -7,27 +7,17 @@
  */
 public class PlayerStatus
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class PlayerStatus
-     */
+    private boolean player = true;
     public PlayerStatus()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public boolean whichPlayer()
     {
-        // put your code here
-        return x + y;
+        player = !player;
+        return player;
+        //x's turn: player == true
+        //0's turn: player == false
     }
 }
